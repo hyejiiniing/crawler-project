0571852029f2afef0bd6929d4ad3c7326aefac8d
hyejiining <hyejiining@gmail.com>
2025-09-23 17:34:40 +0900

    테이블 마이그레이션 추가

--- migrations/20250923080452_create_products_table.js
+++ migrations/20250923080452_create_products_table.js
@@ -2,14 +2,20 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema.createTable("products", (table) => {
+    table.increments("id").primary();
+    table.string("name").notNullable();
+    table.string("price").notNullable();
+    table.text("image_url").notNullable();
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+  });
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema.dropTable("products");
 };
